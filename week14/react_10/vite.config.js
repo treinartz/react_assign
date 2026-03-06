75c03ab845a11635b576bc3c08d33f834f324cb9
treinartz <thomas.reinartz@gmail.com>
2026-03-06 13:13:13 -0600

    Fix base path in Vite configuration

--- week14/react_10/vite.config.js
+++ week14/react_10/vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/react_assign/week14/react10/",
+  base: "/react_assign/week14/react_10/",
 });
